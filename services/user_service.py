5fe6ae23808cad02e707121a570e54038749edc3
mariam0ashraf <mariam00ashraf@gmail.com>
2025-10-29 22:07:10 +0300

    Registration verification

--- services/user_service.py
+++ services/user_service.py
@@ -1,8 +1,17 @@
-from repositories.user_repository import createUser
+from repositories.user_repository import createUser, usersCollection
 from core.security import hashPassword
 
+async def findUserByEmail(email: str):
+    return await usersCollection.find_one({"email": email})
+
 async def registerUser(username: str, email: str, password: str):
-    hashedPass = hashPassword(password)
-    userData = {"username": username, "email": email, "password": hashedPass  }
-    newUser = await createUser(userData)
-    return newUser
+    existingUser = await findUserByEmail(email)
+    if existingUser:
+        return {"error": "User already exists"}
+    try:
+        hashedPass = hashPassword(password)
+        userData = {"username": username, "email": email, "password": hashedPass  }
+        newUser = await createUser(userData)
+        return newUser
+    except Exception :
+        return {"error": "Failed to register user"}
