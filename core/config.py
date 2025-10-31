b3bc5044dc44c1a824075eefe9bbfccabf27c189
Tony-Elia <antony.safaga@gmail.com>
2025-10-31 22:37:51 +0200

    add env vars

--- core/config.py
+++ core/config.py
@@ -1,7 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "event_planning"
+load_dotenv("../.env")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "event_planning")
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
